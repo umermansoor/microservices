310de169e9db42e480042e39995df54e83e0a3f3
Marcelo Freitas <jmfda00@gmail.com>
2019-11-23 13:52:48 -0300

    remove packages version

--- services/showtimes.py
+++ services/showtimes.py
@@ -34,7 +34,7 @@ def showtimes_list():
 def showtimes_record(date):
     if date not in showtimes:
         raise NotFound
-    print showtimes[date]
+    #print(showtimes[date])
     return nice_json(showtimes[date])
 
 # exeuted when this is called from the cmd
