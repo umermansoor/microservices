76961ccdf4a102ff7b2cc4860377a5a6624bbdb7
João Marcelo Freitas de Almeida <jmfda00@gmail.com>
2019-11-26 14:47:42 -0300

    Finish populate_db
    This script cleans and poplates the databases of this application

---
+++ populate_db.py
@@ -0,0 +1,75 @@
+from services.bookings import db as booking_db
+from services.movies import db as movies_db
+from services.rewards import db as rewards_db
+from services.showtimes import db as showtimes_db
+from services.users import db as users_db
+# Models
+from services.bookings import Book
+from services.movies import Movie
+from services.rewards import Reward
+from services.showtimes import Showtime
+from services.users import User
+# timestamp
+from datetime import datetime
+
+# Clean previous databses
+booking_db.drop_all()
+movies_db.drop_all()
+rewards_db.drop_all()
+showtimes_db.drop_all()
+users_db.drop_all()
+
+# Create the databases
+booking_db.create_all()
+movies_db.create_all()
+rewards_db.create_all()
+showtimes_db.create_all()
+users_db.create_all()
+
+# populate bookings
+b1 = Booking(user=1, date=datetime(2019, 11, 1), movie=1)
+b2 = Booking(user=2, date=datetime(2019, 11, 2), movie=2)
+b3 = Booking(user=3, date=datetime(2019, 11, 3), movie=3)
+bookings_db.session.add(b1)
+bookings_db.session.add(b2)
+bookings_db.session.add(b3)
+bookings_db.session.commit()
+
+# populate movies
+m1 = Movies(rating=10, title="Boyhood", director="Richard Linklater")
+m2 = Movies(rating=8, title="Before Sunset", director="Richard Linklater")
+m3 = Movies(rating=9, title="Waking Life", director="Richard Linklater")
+movies_db.session.add(m1)
+movies_db.session.add(m2)
+movies_db.session.add(m3)
+movies_db.session.commit()
+
+
+#populate rewards
+r1 = Reward(user=1, score=0)
+r2 = Reward(user=2, score=0)
+r3 = Reward(user=3, score=0)
+rewards_db.session.add(r1)
+rewards_db.session.add(r2)
+rewards_db.session.add(r3)
+rewards_db.session.commit()
+
+
+#populate showtimes
+s1 = Showtime(date=datetime(2019, 11, 1), movie=1)
+s2 = Showtime(date=datetime(2019, 11, 2), movie=2)
+s3 = Showtime(date=datetime(2019, 11, 3), movie=3)
+showtimes_db.session.add(s1)
+showtimes_db.session.add(s2)
+showtimes_db.session.add(s3)
+showtimes_db.session.commit()
+
+
+# populate users
+u1 = User(name="Jim Halpert")
+u2 = User(name="Dwight Schrute")
+u3 = User(name="Michael Scott")
+users_db.session.add(u1)
+users_db.session.add(u2)
+users_db.session.add(u3)
+users_db.session.commit()
