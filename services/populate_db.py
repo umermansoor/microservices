941dc89ccd3e246afd464284b2f6b7daa112e6f9
marcelo almeida <jmfda00@gmail.com>
2019-11-27 12:01:54 -0200

    populate database properly

--- services/populate_db.py
+++ services/populate_db.py
@@ -1,26 +1,26 @@
-from services.bookings import db as booking_db
-from services.movies import db as movies_db
-from services.rewards import db as rewards_db
-from services.showtimes import db as showtimes_db
-from services.users import db as users_db
+from bookings import db as bookings_db
+from movies import db as movies_db
+from rewards import db as rewards_db
+from showtimes import db as showtimes_db
+from users import db as users_db
 # Models
-from services.bookings import Book
-from services.movies import Movie
-from services.rewards import Reward
-from services.showtimes import Showtime
-from services.users import User
+from bookings import Booking
+from movies import Movie
+from rewards import Reward
+from showtimes import Showtime
+from users import User
 # timestamp
 from datetime import date
 
 # Clean previous databses
-booking_db.drop_all()
+bookings_db.drop_all()
 movies_db.drop_all()
 rewards_db.drop_all()
 showtimes_db.drop_all()
 users_db.drop_all()
 
 # Create the databases
-booking_db.create_all()
+bookings_db.create_all()
 movies_db.create_all()
 rewards_db.create_all()
 showtimes_db.create_all()
@@ -36,9 +36,9 @@ bookings_db.session.add(b3)
 bookings_db.session.commit()
 
 # populate movies
-m1 = Movies(rating=10, title="Boyhood", director="Richard Linklater")
-m2 = Movies(rating=8, title="Before Sunset", director="Richard Linklater")
-m3 = Movies(rating=9, title="Waking Life", director="Richard Linklater")
+m1 = Movie(rating=10, title="Boyhood", director="Richard Linklater")
+m2 = Movie(rating=8, title="Before Sunset", director="Richard Linklater")
+m3 = Movie(rating=9, title="Waking Life", director="Richard Linklater")
 movies_db.session.add(m1)
 movies_db.session.add(m2)
 movies_db.session.add(m3)
